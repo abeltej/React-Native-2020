e8c4b3fa8833315500d6f7b23fdfcf884bba4b9b
adajett <adajett@gmail.com>
2020-12-07 11:22:17 -0700

    .

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const CounterScreen = () => {
 
   return (
     <View>
+      <Text>Current Count</Text>
 
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  // textStyle: {
-  //   fontSize: 30
-  // }
-})
+
+});
 
 export default CounterScreen;
